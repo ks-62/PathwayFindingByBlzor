f1618a17fb510f1db6f1f8a02d23725441f08bf2
ks-62 <71759276+ks-62@users.noreply.github.com>
2022-12-24 22:34:15 +0900

    Add files via upload

---
+++ PathwayFinding/PathwayFinding/Cell.cs
@@ -0,0 +1,39 @@
+﻿namespace PathwayFinding
+{
+    public class Cell
+    {
+        public enum CELL_STATUS
+        {
+            START,
+            END,
+            BARRIER,
+            OPEN,
+            CLOSED,
+            NORMAL
+        }
+
+        public int Row { get; set; }
+
+        public int Col { get; set; }
+
+        public string ElementId { get; set; }
+
+        public string ElementClassName { get; set; }
+
+        public List<Cell> Neighbors { get; set; }
+
+        public CELL_STATUS Status { get; set; }
+
+        public Cell(int argRow, int argCol)
+        {
+            Row = argRow;
+            Col = argCol;
+            Status = CELL_STATUS.NORMAL;
+
+            ElementId = $"{argRow}_{argCol}";
+            ElementClassName = "grid-cell";
+
+        }
+
+    }
+}
---
+++ PathwayFinding/PathwayFinding/wwwroot/js/PathwayFinding.js
@@ -0,0 +1,36 @@
+﻿
+function MakeBarrier(cellId) {
+    var e = document.getElementById(cellId);
+    e.classList.add("barrier");
+}
+
+function MakeStart(cellId) {
+    var e = document.getElementById(cellId);
+    e.classList.add("start");
+}
+
+function MakeEnd(cellId) {
+    var e = document.getElementById(cellId);
+    e.classList.add("end");
+}
+
+function MakeOpen(cellId) {
+    var e = document.getElementById(cellId);
+    e.classList.add("open");
+}
+
+function MakeClosed(cellId) {
+    var e = document.getElementById(cellId);
+    e.classList.add("closed");
+}
+
+function MakePath(cellId) {
+    var e = document.getElementById(cellId);
+    e.classList.add("path");
+}
+
+function CheckClass(cellId, cellStatus) {
+    let status = document.getElementById(cellId).classList.contains(cellStatus);
+    console.log(status);
+    return status;
+}
